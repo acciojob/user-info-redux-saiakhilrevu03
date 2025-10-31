61311e2aa2e435d6d7b2e242a3d481729d03428a
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-31 21:55:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
